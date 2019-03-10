a6f212fe34fcc738ef494bc823aded01a8d25b9f
Walter Schweitzer <wschweitzer00@gmail.com>
2019-03-10 09:56:31 -0400

    update temperature api call

--- api/resources/temperature.py
+++ api/resources/temperature.py
@@ -7,17 +7,21 @@ from ..src.temperature_conversion import TemperatureConversion as tc
 @app.route('/temperature', methods = ['POST'])
 @swag_from('../docs/temperature_conversion.yml')
 def temperature_conversion():
-    """ Temperature Conversion """
+    """ Temperature Conversion POST """
 
+    ## get post parameters
     conversion_type = request.data.get('conversion_type')
     input_temp = request.data.get('input_temp')
     student_response = request.data.get('student_response')
 
+    ## use tc class to call correct function to do the conversion
     tc_obj = tc()
     function_to_call = getattr(tc_obj, conversion_type)
-    result = function_to_call(input_temp)
+    conversion_result = function_to_call(input_temp)
 
+    ## return the response
     data = {}
-    data['result'] = result
+    data['conversion_result'] = conversion_result
+
 
     return data
